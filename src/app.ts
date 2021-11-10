26c32a2194c9ec55fa747f875dc4080edb4fbc80
LAPTOP-67AOG24S\dnavi <dnavin497@gmail.com>
2021-11-10 10:11:33 +0530

    allow cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express, {json, urlencoded} from 'express';
+import express, { json, urlencoded } from 'express';
 import { createServer } from "http";
+import cors from 'cors'
 import { Server } from "socket.io";
 import pingRoute from './ping';
 
@@ -10,15 +11,21 @@ const app = express();
 const port = 8000;
 app.use(urlencoded({ extended: true }));
 app.use(json());
+app.use(cors());
 
 app.use('/stickyNotes', stickyNotesRoute);
 app.use('/ping', pingRoute)
 
 const httpServer = createServer(app);
-export const io = new Server(httpServer, { /* options */ });
+export const io = new Server(httpServer, {
+  cors: {
+    origin: "ec2-50-17-162-1.compute-1.amazonaws.com",
+    methods: ["GET", "POST"]
+  }
+});
 
 io.on("connection", (socket) => {
   onConnection(socket);
 });
 
-httpServer.listen(port,()=>{console.log(`server listening on port ${port}`)});
+httpServer.listen(port, () => { console.log(`server listening on port ${port}`) });
